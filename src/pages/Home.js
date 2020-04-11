b13a1de56532a59490c0d1088d99d5b6cf2aa288
Daniel Santos <Dsantos0495@gmail.com>
2020-04-11 17:01:26 -0500

    Listo para deploy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,24 @@
-const Home = () => {
+import getData from '../utils/getData';
+
+const Home = async () => {
+    const characters = await getData();
+//usamos el metodo .map de para iterar cada persona con el template y así se crea un array igual 
+//que el template pero para cada personaje
     const view = `
         <div class="Characters">
-            <article class="Character-item">
-                <a href="#/1/">
-                    <img src="image" alt="name"/>
-                    <h2>Name</h2>
-                </a>
-            </article>
+            ${characters.results.map(character => `
+                <article class="Character-item">
+                    <a href="#/${character.id}/">
+                        <img src="${character.image}" alt="${character.name}"/>
+                        <h2>${character.name}</h2>
+                    </a>
+                </article>
+            `).join('')}
+
         </div>
     `;
+    //usamos .join('') al final porque nos devuelve un array y necesitamos que todo este junto
+    //porque los array usamo comillas para separa los elementos para el render join hace eso
     return view
 };
 
